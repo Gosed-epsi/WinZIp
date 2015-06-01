d7e9dcd289d676ed524b9eb4f585820fe79001aa
Stéphane Trognon <trognon.stephane@gmail.com>
2015-06-01 22:10:04 +0200

    Ajout de la compression, et de la decompression + todo sur comrpession problème fichier corrompu

--- main.cpp
+++ main.cpp
@@ -16,20 +16,43 @@ void run_zipper(const QString &fileZip)
         QFile file(pathFile);
         //Definition de l'ouverture (obligatoire sinon pas d'accès)
         file.open(QIODevice::ReadOnly | QIODevice::Text);
-        //TextStream permet de lire le fichier (à changer par un récupérateur de bytes)
-        QTextStream in(&file);
-        //Lit toutes les lignes
-        QString f=in.readAll();
-        //iosstream affichage du contenu
-        std::cout << pathFile.toStdString() << f.toStdString() << std::endl;
+        //Recuperation des bytes
+        QByteArray bytesFile = file.readAll();
+        //Bytes compresser
+        QByteArray bytesCompress = qCompress(bytesFile);
+        //fichier de bytes
+        QFile fileCompress("/Users/sinys/EPSI/IA/Clip/test.zip");
+        //Ouverture
+        fileCompress.open(QIODevice::WriteOnly | QIODevice::Text);
+        //Ecriture
+        fileCompress.write(bytesCompress);
         //fermeture du fichier
         file.close();
+        fileCompress.close();
+        break;
     }
 }
 
 
 void run_dezipper(const QString &fileDezip){
 
+    //fichier de bytes
+    QFile fileCompress("/Users/sinys/EPSI/IA/Clip/test.zip");
+    //Ouverture
+    fileCompress.open(QIODevice::ReadOnly | QIODevice::Text);
+    //Recuperation des bytes
+    QByteArray bytesFile = fileCompress.readAll();
+    //Bytes decompresser
+    QByteArray bytesCompress = qUncompress(bytesFile);
+    //Création d'un file
+    QFile file("/Users/sinys/EPSI/IA/Clip/clp.clp");
+    //Definition de l'ouverture (obligatoire sinon pas d'accès)
+    file.open(QIODevice::WriteOnly | QIODevice::Text);
+    //Ecriture
+    file.write(bytesCompress);
+    //fermeture du fichier
+    file.close();
+    fileCompress.close();
 }
 
 int main(int argc, char *argv[])
